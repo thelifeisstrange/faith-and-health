897f96b0de2ba46edd34465149a9eb22ca2d2aa3
thelifeisstrange <yogeshpk0123@gmail.com>
2024-01-13 19:49:49 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './pages/Home';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Services from './pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <BrowserRouter>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<Home />}/>  
+            </Routes>  
+          </BrowserRouter>
   );
 }
 
